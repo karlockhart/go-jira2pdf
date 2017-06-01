fef7ebad18cc08b84b07efed27f0d73d755a3791
Tim Allen <tlallen@us.ibm.com>
2017-06-01 10:52:31 -0500

    Change feature to get all issues in a project. Allow fetching of multiple projects.

--- pkg/jira2pdf/jira2pdf.go
+++ pkg/jira2pdf/jira2pdf.go
@@ -5,15 +5,10 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"strconv"
-	"strings"
-	"time"
-
-	"golang.org/x/text/encoding/charmap"
 
 	jira "github.com/andygrunwald/go-jira"
-	"github.com/jung-kurt/gofpdf"
 	"github.com/karlockhart/go-jira2pdf/pkg/config"
+	"github.com/karlockhart/go-jira2pdf/pkg/pdf"
 	"github.com/spf13/viper"
 )
 
@@ -26,73 +21,55 @@ func RunJira2PDF(configFile string) {
 
 	jiraClient, err := buildJiraClient()
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("Failed to crate jira client: %v", err)
 	}
 
-	searchOptions := &jira.SearchOptions{
-		MaxResults: -1,
+	jiraProjectKeys := viper.GetStringSlice("jira_project_keys")
+	if len(jiraProjectKeys) == 0 {
+		jiraProjectKeys, err = getAlljiraProjectKeys(jiraClient)
+		if err != nil {
+			log.Fatalf("Failed to all jira project ids: %v", err)
+		}
 	}
 
-	issues, _, err := jiraClient.Issue.Search(viper.GetString("jql_query"), searchOptions)
+	projectCount := len(jiraProjectKeys)
+	fmt.Printf("Getting ready to build %d PDFs\n", projectCount)
+
+	//Build PDFs for each project with all issues
+	for i, projectKey := range jiraProjectKeys {
+		jqlQuery := fmt.Sprintf("project = '%s'", projectKey)
+
+		fmt.Printf("%d/%d Getting issues for %s.pdf\n", i+1, projectCount, projectKey)
+		issues, _, err := jiraClient.Issue.Search(
+			jqlQuery,
+			&jira.SearchOptions{MaxResults: -1},
+		)
+		if err != nil {
+			log.Fatalf("Issue query failed: %v", err)
+		}
+
+		fmt.Printf("%d/%d Building %s.pdf...\n", i+1, projectCount, projectKey)
+		err = pdf.Build(projectKey, issues)
+		if err != nil {
+			log.Fatalf("Build PDF failed: %v", err)
+		}
+		fmt.Printf("%d/%d %s.pdf complete\n", i+1, projectCount, projectKey)
+	}
+
+}
+
+func getAlljiraProjectKeys(jiraClient *jira.Client) ([]string, error) {
+	var projects []string
+	projectList, _, err := jiraClient.Project.GetList()
 	if err != nil {
-		log.Fatalf("Issue query failed: %v", err)
+		return nil, err
 	}
 
-	// generate PDF
-	pdfGenerator := gofpdf.New("P", "mm", "A4", "")
-	pdfTR := pdfGenerator.UnicodeTranslatorFromDescriptor("")
-
-	pdfGenerator.AddPage()
-
-	// document title
-	pdfGenerator.SetFont("Arial", "B", 16)
-	pdfGenerator.SetFillColor(222, 222, 222)
-	pdfGenerator.SetTextColor(0, 0, 0)
-	pdfGenerator.MultiCell(0, 16, pdfTR("JIRA Issues"), "1", "C", true)
-
-	// document subtitle
-	currentPosY := pdfGenerator.GetY()
-	pdfGenerator.SetFont("Arial", "", 9)
-	pdfGenerator.SetTextColor(0, 0, 0)
-	pdfGenerator.MultiCell(0, 9, fmt.Sprintf("Total of issues: %v", len(issues)), "1", "L", false)
-	pdfGenerator.SetY(currentPosY)
-	pdfGenerator.MultiCell(0, 9, fmt.Sprintf("Created at: %v", time.Now().Format(viper.GetString("datetime_format"))), "1", "R", false)
-	pdfGenerator.Ln(4)
-
-	// issues
-	for index, issue := range issues {
-		// set issue font
-		var lineHt float64 = 9
-		pdfGenerator.SetFont("Arial", "", lineHt)
-		pdfGenerator.SetTextColor(0, 0, 0)
-
-		// parse issue template
-		issueText := parseIssueTemplate(index, issue)
-		issueText, _ = charmap.Windows1252.NewEncoder().String(issueText)
-
-		html := pdfGenerator.HTMLBasicNew()
-		html.Write(lineHt, issueText)
-
-		// draw issue separator
-		pdfGenerator.SetDrawColor(195, 195, 195)
-		pdfGenerator.Ln(lineHt)
-		pdfGenerator.Ln(2)
-
-		pageWidth, _ := pdfGenerator.GetPageSize()
-		x, y := pdfGenerator.GetXY()
-		marginL, marginR, _, _ := pdfGenerator.GetMargins()
-		pdfGenerator.Line(x, y, x+pageWidth-marginR-marginL, y)
-
-		pdfGenerator.Ln(2)
+	for _, project := range *projectList {
+		projects = append(projects, project.Key)
 	}
 
-	// save to output filename
-	err = pdfGenerator.OutputFileAndClose("Test.pdf")
-
-	if err != nil {
-		log.Fatalf("Erro while save PDF: %v", err)
-	}
-
+	return projects, nil
 }
 
 func buildJiraClient() (*jira.Client, error) {
@@ -110,112 +87,4 @@ func buildJiraClient() (*jira.Client, error) {
 	jiraClient.Authentication.SetBasicAuth(viper.GetString("J2P_USERNAME"), viper.GetString("J2P_PASSWORD"))
 
 	return jiraClient, nil
-}
-
-func parseIssueTemplate(index int, issue jira.Issue) string {
-	issueText := viper.GetString("jira_issue_template")
-
-	issueText = strings.Replace(issueText, "[issue.key]", issue.Key, -1)
-	issueText = strings.Replace(issueText, "[issue.id]", issue.ID, -1)
-	issueText = strings.Replace(issueText, "[issue.fields.description]", issue.Fields.Description, -1)
-	issueText = strings.Replace(issueText, "[issue.fields.duedate]", issue.Fields.Duedate, -1)
-	issueText = strings.Replace(issueText, "[issue.fields.expand]", issue.Fields.Expand, -1)
-	issueText = strings.Replace(issueText, "[issue.fields.resolutiondate]", issue.Fields.Resolutiondate, -1)
-	issueText = strings.Replace(issueText, "[issue.fields.summary]", issue.Fields.Summary, -1)
-	issueText = strings.Replace(issueText, "[issue.fields.timeestimate]", strconv.Itoa(issue.Fields.TimeEstimate), -1)
-	issueText = strings.Replace(issueText, "[issue.fields.timeoriginalestimate]", strconv.Itoa(issue.Fields.TimeOriginalEstimate), -1)
-	issueText = strings.Replace(issueText, "[issue.fields.timespent]", strconv.Itoa(issue.Fields.TimeSpent), -1)
-
-	issueText = strings.Replace(issueText, "[issue.fields.project.name]", issue.Fields.Project.Name, -1)
-	issueText = strings.Replace(issueText, "[issue.fields.project.description]", issue.Fields.Project.Description, -1)
-	issueText = strings.Replace(issueText, "[issue.fields.project.id]", issue.Fields.Project.ID, -1)
-	issueText = strings.Replace(issueText, "[issue.fields.project.key]", issue.Fields.Project.Key, -1)
-
-	issueText = strings.Replace(issueText, "[issue.fields.type.name]", issue.Fields.Type.Name, -1)
-	issueText = strings.Replace(issueText, "[issue.fields.type.description]", issue.Fields.Type.Description, -1)
-	issueText = strings.Replace(issueText, "[issue.fields.type.id]", issue.Fields.Type.ID, -1)
-
-	if issue.Fields.Priority != nil {
-		issueText = strings.Replace(issueText, "[issue.fields.priority.id]", issue.Fields.Priority.ID, -1)
-		issueText = strings.Replace(issueText, "[issue.fields.priority.name]", issue.Fields.Priority.Name, -1)
-	} else {
-		issueText = strings.Replace(issueText, "[issue.fields.priority.id]", "", -1)
-		issueText = strings.Replace(issueText, "[issue.fields.priority.name]", "", -1)
-	}
-
-	if issue.Fields.AggregateProgress != nil {
-		issueText = strings.Replace(issueText, "[issue.fields.aggregateprogress.progress]", strconv.Itoa(issue.Fields.AggregateProgress.Progress), -1)
-		issueText = strings.Replace(issueText, "[issue.fields.aggregateprogress.total]", strconv.Itoa(issue.Fields.AggregateProgress.Total), -1)
-	} else {
-		issueText = strings.Replace(issueText, "[issue.fields.aggregateprogress.progress]", "", -1)
-		issueText = strings.Replace(issueText, "[issue.fields.aggregateprogress.total]", "", -1)
-	}
-
-	if issue.Fields.Progress != nil {
-		issueText = strings.Replace(issueText, "[issue.fields.progress.progress]", strconv.Itoa(issue.Fields.Progress.Progress), -1)
-		issueText = strings.Replace(issueText, "[issue.fields.progress.total]", strconv.Itoa(issue.Fields.Progress.Total), -1)
-	} else {
-		issueText = strings.Replace(issueText, "[issue.fields.progress.progress]", "", -1)
-		issueText = strings.Replace(issueText, "[issue.fields.progress.total]", "", -1)
-	}
-
-	if issue.Fields.Assignee != nil {
-		issueText = strings.Replace(issueText, "[issue.fields.assignee.name]", issue.Fields.Assignee.Name, -1)
-		issueText = strings.Replace(issueText, "[issue.fields.assignee.emailaddrress]", issue.Fields.Assignee.EmailAddress, -1)
-		issueText = strings.Replace(issueText, "[issue.fields.assignee.displayname]", issue.Fields.Assignee.DisplayName, -1)
-		issueText = strings.Replace(issueText, "[issue.fields.assignee.key]", issue.Fields.Assignee.Key, -1)
-	} else {
-		issueText = strings.Replace(issueText, "[issue.fields.assignee.name]", "", -1)
-		issueText = strings.Replace(issueText, "[issue.fields.assignee.emailaddrress]", "", -1)
-		issueText = strings.Replace(issueText, "[issue.fields.assignee.displayname]", "", -1)
-		issueText = strings.Replace(issueText, "[issue.fields.assignee.key]", "", -1)
-	}
-
-	if issue.Fields.Creator != nil {
-		issueText = strings.Replace(issueText, "[issue.fields.creator.name]", issue.Fields.Creator.Name, -1)
-		issueText = strings.Replace(issueText, "[issue.fields.creator.emailaddrress]", issue.Fields.Creator.EmailAddress, -1)
-		issueText = strings.Replace(issueText, "[issue.fields.creator.displayname]", issue.Fields.Creator.DisplayName, -1)
-		issueText = strings.Replace(issueText, "[issue.fields.creator.key]", issue.Fields.Creator.Key, -1)
-	} else {
-		issueText = strings.Replace(issueText, "[issue.fields.creator.name]", "", -1)
-		issueText = strings.Replace(issueText, "[issue.fields.creator.emailaddrress]", "", -1)
-		issueText = strings.Replace(issueText, "[issue.fields.creator.displayname]", "", -1)
-		issueText = strings.Replace(issueText, "[issue.fields.creator.key]", "", -1)
-	}
-
-	if issue.Fields.Reporter != nil {
-		issueText = strings.Replace(issueText, "[issue.fields.reporter.name]", issue.Fields.Reporter.Name, -1)
-		issueText = strings.Replace(issueText, "[issue.fields.reporter.emailaddrress]", issue.Fields.Reporter.EmailAddress, -1)
-		issueText = strings.Replace(issueText, "[issue.fields.reporter.displayname]", issue.Fields.Reporter.DisplayName, -1)
-		issueText = strings.Replace(issueText, "[issue.fields.reporter.key]", issue.Fields.Reporter.Key, -1)
-	} else {
-		issueText = strings.Replace(issueText, "[issue.fields.reporter.name]", "", -1)
-		issueText = strings.Replace(issueText, "[issue.fields.reporter.emailaddrress]", "", -1)
-		issueText = strings.Replace(issueText, "[issue.fields.reporter.displayname]", "", -1)
-		issueText = strings.Replace(issueText, "[issue.fields.reporter.key]", "", -1)
-	}
-
-	if issue.Fields.Status != nil {
-		issueText = strings.Replace(issueText, "[issue.fields.status.name]", issue.Fields.Status.Name, -1)
-		issueText = strings.Replace(issueText, "[issue.fields.status.description]", issue.Fields.Status.Description, -1)
-		issueText = strings.Replace(issueText, "[issue.fields.status.id]", issue.Fields.Status.ID, -1)
-	} else {
-		issueText = strings.Replace(issueText, "[issue.fields.status.name]", "", -1)
-		issueText = strings.Replace(issueText, "[issue.fields.status.description]", "", -1)
-		issueText = strings.Replace(issueText, "[issue.fields.status.id]", "", -1)
-	}
-
-	if len(issue.Fields.Created) > 0 {
-		dateTime, err := time.Parse(viper.GetString("api_datetime_format"), issue.Fields.Created)
-
-		if err == nil {
-			issueText = strings.Replace(issueText, "[issue.fields.created]", dateTime.Format(viper.GetString("datetime_format")), -1)
-		} else {
-			log.Printf("Error on parse field \"created\"! %v\n", err)
-
-			issueText = strings.Replace(issueText, "[issue.fields.created]", "", -1)
-		}
-	}
-
-	return issueText
 }
---
+++ pkg/pdf/pdf.go
@@ -0,0 +1,184 @@
+package pdf
+
+import (
+	"errors"
+	"fmt"
+	"log"
+	"strconv"
+	"strings"
+	"time"
+
+	"golang.org/x/text/encoding/charmap"
+
+	jira "github.com/andygrunwald/go-jira"
+	"github.com/jung-kurt/gofpdf"
+	"github.com/spf13/viper"
+)
+
+//Build a pdf from jira issues
+func Build(project string, issues []jira.Issue) error {
+	pdfGenerator := gofpdf.New("P", "mm", "A4", "")
+	pdfTR := pdfGenerator.UnicodeTranslatorFromDescriptor("")
+
+	pdfGenerator.AddPage()
+
+	// document title
+	pdfGenerator.SetFont("Arial", "B", 16)
+	pdfGenerator.SetFillColor(222, 222, 222)
+	pdfGenerator.SetTextColor(0, 0, 0)
+	pdfGenerator.MultiCell(0, 16, pdfTR(project+" Issues"), "1", "C", true)
+
+	// document subtitle
+	currentPosY := pdfGenerator.GetY()
+	pdfGenerator.SetFont("Arial", "", 9)
+	pdfGenerator.SetTextColor(0, 0, 0)
+	pdfGenerator.MultiCell(0, 9, fmt.Sprintf("Total of issues: %v", len(issues)), "1", "L", false)
+	pdfGenerator.SetY(currentPosY)
+	pdfGenerator.MultiCell(0, 9, fmt.Sprintf("Created at: %v", time.Now().Format(viper.GetString("datetime_format"))), "1", "R", false)
+	pdfGenerator.Ln(4)
+
+	//issues
+	for _, issue := range issues {
+		// set issue font
+		var lineHt float64 = 9
+		pdfGenerator.SetFont("Arial", "", lineHt)
+		pdfGenerator.SetTextColor(0, 0, 0)
+
+		// parse issue template
+		issueText := parseIssueTemplate(issue)
+		issueText, _ = charmap.Windows1252.NewEncoder().String(issueText)
+
+		html := pdfGenerator.HTMLBasicNew()
+		html.Write(lineHt, issueText)
+
+		// draw issue separator
+		pdfGenerator.SetDrawColor(195, 195, 195)
+		pdfGenerator.Ln(lineHt)
+		pdfGenerator.Ln(2)
+
+		pageWidth, _ := pdfGenerator.GetPageSize()
+		x, y := pdfGenerator.GetXY()
+		marginL, marginR, _, _ := pdfGenerator.GetMargins()
+		pdfGenerator.Line(x, y, x+pageWidth-marginR-marginL, y)
+
+		pdfGenerator.Ln(2)
+	}
+
+	// save to output filename
+	err := pdfGenerator.OutputFileAndClose(project + ".pdf")
+
+	if err != nil {
+		errString := fmt.Sprintf("Erro while saving %s.pdf: %v", project, err)
+		return errors.New(errString)
+	}
+
+	return nil
+}
+
+func parseIssueTemplate(issue jira.Issue) string {
+	issueText := viper.GetString("jira_issue_template")
+
+	issueText = strings.Replace(issueText, "[issue.key]", issue.Key, -1)
+	issueText = strings.Replace(issueText, "[issue.id]", issue.ID, -1)
+	issueText = strings.Replace(issueText, "[issue.fields.description]", issue.Fields.Description, -1)
+	issueText = strings.Replace(issueText, "[issue.fields.duedate]", issue.Fields.Duedate, -1)
+	issueText = strings.Replace(issueText, "[issue.fields.expand]", issue.Fields.Expand, -1)
+	issueText = strings.Replace(issueText, "[issue.fields.resolutiondate]", issue.Fields.Resolutiondate, -1)
+	issueText = strings.Replace(issueText, "[issue.fields.summary]", issue.Fields.Summary, -1)
+	issueText = strings.Replace(issueText, "[issue.fields.timeestimate]", strconv.Itoa(issue.Fields.TimeEstimate), -1)
+	issueText = strings.Replace(issueText, "[issue.fields.timeoriginalestimate]", strconv.Itoa(issue.Fields.TimeOriginalEstimate), -1)
+	issueText = strings.Replace(issueText, "[issue.fields.timespent]", strconv.Itoa(issue.Fields.TimeSpent), -1)
+
+	issueText = strings.Replace(issueText, "[issue.fields.project.name]", issue.Fields.Project.Name, -1)
+	issueText = strings.Replace(issueText, "[issue.fields.project.description]", issue.Fields.Project.Description, -1)
+	issueText = strings.Replace(issueText, "[issue.fields.project.id]", issue.Fields.Project.ID, -1)
+	issueText = strings.Replace(issueText, "[issue.fields.project.key]", issue.Fields.Project.Key, -1)
+
+	issueText = strings.Replace(issueText, "[issue.fields.type.name]", issue.Fields.Type.Name, -1)
+	issueText = strings.Replace(issueText, "[issue.fields.type.description]", issue.Fields.Type.Description, -1)
+	issueText = strings.Replace(issueText, "[issue.fields.type.id]", issue.Fields.Type.ID, -1)
+
+	if issue.Fields.Priority != nil {
+		issueText = strings.Replace(issueText, "[issue.fields.priority.id]", issue.Fields.Priority.ID, -1)
+		issueText = strings.Replace(issueText, "[issue.fields.priority.name]", issue.Fields.Priority.Name, -1)
+	} else {
+		issueText = strings.Replace(issueText, "[issue.fields.priority.id]", "", -1)
+		issueText = strings.Replace(issueText, "[issue.fields.priority.name]", "", -1)
+	}
+
+	if issue.Fields.AggregateProgress != nil {
+		issueText = strings.Replace(issueText, "[issue.fields.aggregateprogress.progress]", strconv.Itoa(issue.Fields.AggregateProgress.Progress), -1)
+		issueText = strings.Replace(issueText, "[issue.fields.aggregateprogress.total]", strconv.Itoa(issue.Fields.AggregateProgress.Total), -1)
+	} else {
+		issueText = strings.Replace(issueText, "[issue.fields.aggregateprogress.progress]", "", -1)
+		issueText = strings.Replace(issueText, "[issue.fields.aggregateprogress.total]", "", -1)
+	}
+
+	if issue.Fields.Progress != nil {
+		issueText = strings.Replace(issueText, "[issue.fields.progress.progress]", strconv.Itoa(issue.Fields.Progress.Progress), -1)
+		issueText = strings.Replace(issueText, "[issue.fields.progress.total]", strconv.Itoa(issue.Fields.Progress.Total), -1)
+	} else {
+		issueText = strings.Replace(issueText, "[issue.fields.progress.progress]", "", -1)
+		issueText = strings.Replace(issueText, "[issue.fields.progress.total]", "", -1)
+	}
+
+	if issue.Fields.Assignee != nil {
+		issueText = strings.Replace(issueText, "[issue.fields.assignee.name]", issue.Fields.Assignee.Name, -1)
+		issueText = strings.Replace(issueText, "[issue.fields.assignee.emailaddrress]", issue.Fields.Assignee.EmailAddress, -1)
+		issueText = strings.Replace(issueText, "[issue.fields.assignee.displayname]", issue.Fields.Assignee.DisplayName, -1)
+		issueText = strings.Replace(issueText, "[issue.fields.assignee.key]", issue.Fields.Assignee.Key, -1)
+	} else {
+		issueText = strings.Replace(issueText, "[issue.fields.assignee.name]", "", -1)
+		issueText = strings.Replace(issueText, "[issue.fields.assignee.emailaddrress]", "", -1)
+		issueText = strings.Replace(issueText, "[issue.fields.assignee.displayname]", "", -1)
+		issueText = strings.Replace(issueText, "[issue.fields.assignee.key]", "", -1)
+	}
+
+	if issue.Fields.Creator != nil {
+		issueText = strings.Replace(issueText, "[issue.fields.creator.name]", issue.Fields.Creator.Name, -1)
+		issueText = strings.Replace(issueText, "[issue.fields.creator.emailaddrress]", issue.Fields.Creator.EmailAddress, -1)
+		issueText = strings.Replace(issueText, "[issue.fields.creator.displayname]", issue.Fields.Creator.DisplayName, -1)
+		issueText = strings.Replace(issueText, "[issue.fields.creator.key]", issue.Fields.Creator.Key, -1)
+	} else {
+		issueText = strings.Replace(issueText, "[issue.fields.creator.name]", "", -1)
+		issueText = strings.Replace(issueText, "[issue.fields.creator.emailaddrress]", "", -1)
+		issueText = strings.Replace(issueText, "[issue.fields.creator.displayname]", "", -1)
+		issueText = strings.Replace(issueText, "[issue.fields.creator.key]", "", -1)
+	}
+
+	if issue.Fields.Reporter != nil {
+		issueText = strings.Replace(issueText, "[issue.fields.reporter.name]", issue.Fields.Reporter.Name, -1)
+		issueText = strings.Replace(issueText, "[issue.fields.reporter.emailaddrress]", issue.Fields.Reporter.EmailAddress, -1)
+		issueText = strings.Replace(issueText, "[issue.fields.reporter.displayname]", issue.Fields.Reporter.DisplayName, -1)
+		issueText = strings.Replace(issueText, "[issue.fields.reporter.key]", issue.Fields.Reporter.Key, -1)
+	} else {
+		issueText = strings.Replace(issueText, "[issue.fields.reporter.name]", "", -1)
+		issueText = strings.Replace(issueText, "[issue.fields.reporter.emailaddrress]", "", -1)
+		issueText = strings.Replace(issueText, "[issue.fields.reporter.displayname]", "", -1)
+		issueText = strings.Replace(issueText, "[issue.fields.reporter.key]", "", -1)
+	}
+
+	if issue.Fields.Status != nil {
+		issueText = strings.Replace(issueText, "[issue.fields.status.name]", issue.Fields.Status.Name, -1)
+		issueText = strings.Replace(issueText, "[issue.fields.status.description]", issue.Fields.Status.Description, -1)
+		issueText = strings.Replace(issueText, "[issue.fields.status.id]", issue.Fields.Status.ID, -1)
+	} else {
+		issueText = strings.Replace(issueText, "[issue.fields.status.name]", "", -1)
+		issueText = strings.Replace(issueText, "[issue.fields.status.description]", "", -1)
+		issueText = strings.Replace(issueText, "[issue.fields.status.id]", "", -1)
+	}
+
+	if len(issue.Fields.Created) > 0 {
+		dateTime, err := time.Parse(viper.GetString("api_datetime_format"), issue.Fields.Created)
+
+		if err == nil {
+			issueText = strings.Replace(issueText, "[issue.fields.created]", dateTime.Format(viper.GetString("datetime_format")), -1)
+		} else {
+			log.Printf("Error on parse field \"created\"! %v\n", err)
+
+			issueText = strings.Replace(issueText, "[issue.fields.created]", "", -1)
+		}
+	}
+
+	return issueText
+}
